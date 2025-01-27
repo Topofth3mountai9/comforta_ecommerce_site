ed00b1cdcc08d5856b28147672688db7aaa5e9e8
Topofthemountain <jefferymiles333@gmail.com>
2025-01-27 09:34:57 +0300

    MAKE AUTH USE LIVE SUPABASE INSTANCE

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'http://127.0.0.1:54321';
+const supabaseUrl = 'https://cyyjhqrlnaglnvlehyvc.supabase.co';
 const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5eWpocXJsbmFnbG52bGVoeXZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5NTg3MTIsImV4cCI6MjA1MzUzNDcxMn0.TPYIH9AXu5ikz0Oh3vuIjBy5kvvBdBMdpLyw2bziL1g';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
